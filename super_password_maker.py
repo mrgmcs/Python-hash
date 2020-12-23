ab6c8e6d41c0b87e76dcddda318b1aa0eac84a34
Moein Rezaie <72414710+mrgmcs@users.noreply.github.com>
2020-12-24 00:29:13 +0330

    Update super_password_maker.py

--- super_password_maker.py
+++ super_password_maker.py
@@ -45,16 +45,21 @@ def func1(Key):
 		half_of_ascy= ascy_for/2
 
 		#after this line is incomprehensible:)
+		#from 1st to 32th char using this code to hash again
 		if int_of_i <= half_of_ascy:
 			pass_index= (ord(i[-1])) % 10
 			salt_index= (ord(i[-1])) % 10
 			sha256_key = sha256_key.replace(sha256_key[pass_index], salt_list[salt_index])
+		#from 32th to end(64th) using this code to hash	
 		else :
 			pass_index= ((ord(i[0]))**mod_i_10) % 10
 			salt_index= ((ord(i[0]))**mod_i_10) % 10
 			sha256_key = sha256_key.replace(sha256_key[-1*pass_index], salt_list[-1*salt_index])
-
-		#return sha256_key
+			#pass_index and salt_index has (mod_i_10), this syntax worte to make a different number, you can put your- 
+			#own number like(10), its not important.
+			
+			#I put a (-1) before my indexes , it will read my char from last to first(unlike previos one(line 49-53).
+		
 get_lenth()
 func1(User_key)
 
